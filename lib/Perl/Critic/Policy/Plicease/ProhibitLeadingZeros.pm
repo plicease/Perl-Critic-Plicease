97f1066d09c0f7f9f7bf65ed8b2db69050ccade3
Graham Ollis <plicease@cpan.org>
2024-02-10 05:27:29 -0700

    require Perl 5.10.1 (same as current Perl::Critic)

--- lib/Perl/Critic/Policy/Plicease/ProhibitLeadingZeros.pm
+++ lib/Perl/Critic/Policy/Plicease/ProhibitLeadingZeros.pm
@@ -2,7 +2,7 @@ package Perl::Critic::Policy::Plicease::ProhibitLeadingZeros;
 
 use strict;
 use warnings;
-use 5.008004;
+use 5.010001;
 use Perl::Critic::Utils;
 use base qw( Perl::Critic::Policy );
 
